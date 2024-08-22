a0ef76b55c249815eda1e189316eaf978353a902
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2024-08-22 19:58:59 -0300

    add validators

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import { screen } from './objects/screen.js';
 
 document.getElementById('btn-search').addEventListener('click', () => {
   const userName = document.getElementById('input-search').value
+  if(validateEmptyInput(userName)) return
   getUserData(userName)
 });
 
@@ -14,16 +15,32 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
   const isEnterKeyPressed = key === 13
 
   if (isEnterKeyPressed) {
+    if(validateEmptyInput(userName)) return
     getUserData(userName)
   }
 });
 
 async function getUserData(userName) {
   const userResponse = await getUser(userName)
+  if(validateUserExistenceEmpty(userResponse) === true) return
   const repositoriesResponse = await getRepositories(userName)
-  
+
   user.setInfo(userResponse)
   user.setRepositories(repositoriesResponse)
 
   screen.renderUser(user)
+}
+
+function validateUserExistenceEmpty(userResponse) {
+  if(userResponse.message === "Not Found") {
+    screen.renderNotFound()
+    return true
+  }
+}
+
+function validateEmptyInput(userName) {
+  if(userName.length === 0) { 
+    alert('Preencha o campo com algum nome') 
+    return true
+  }
 }
